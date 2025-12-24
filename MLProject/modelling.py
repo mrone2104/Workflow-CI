0dae02206880a12baf5cf6db2ef4718b368368c8
mrone2104 <116262378+mrone2104@users.noreply.github.com>
2025-12-24 16:04:52 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,52 +1,70 @@
 import pandas as pd
+import mlflow
+import mlflow.xgboost
 import xgboost as xgb
-import joblib
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, f1_score
 
 
-# ======================================================
-# LOAD DATA
-# ======================================================
-df = pd.read_csv("data_preprocessed.csv")
+# ❌ JANGAN set_tracking_uri di CI (AMAN DIHAPUS)
+# mlflow.set_tracking_uri("file:./mlruns")
 
-DROP_COLS = ["customer_id", "customer_id_encoded", "target_offer"]
+EXPERIMENT_NAME = "Telco_Offer_Basic"
+mlflow.set_experiment(EXPERIMENT_NAME)
+
+
+DATA_PATH = "data_preprocessed.csv"
+df = pd.read_csv(DATA_PATH)
+
+
+DROP_COLS = [
+    "customer_id",
+    "customer_id_encoded",
+    "target_offer"
+]
+
 X = df.drop(columns=[c for c in DROP_COLS if c in df.columns])
 y = df["target_offer_encoded"].astype(int)
 
+
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, stratify=y, random_state=42
+    X,
+    y,
+    test_size=0.2,
+    stratify=y,
+    random_state=42
 )
 
 NUM_CLASS = y.nunique()
 
 
-# ======================================================
-# TRAIN MODEL
-# ======================================================
-model = xgb.XGBClassifier(
-    n_estimators=200,
-    max_depth=7,
-    learning_rate=0.05,
-    objective="multi:softprob",
-    num_class=NUM_CLASS,
-    eval_metric="mlogloss",
-    random_state=42
-)
+with mlflow.start_run(run_name="XGBoost_Basic"):
 
-model.fit(X_train, y_train)
+    model = xgb.XGBClassifier(
+        n_estimators=200,
+        max_depth=7,
+        learning_rate=0.05,
+        objective="multi:softprob",
+        num_class=NUM_CLASS,
+        eval_metric="mlogloss",
+        random_state=42
+    )
 
-y_pred = model.predict(X_test)
-acc = accuracy_score(y_test, y_pred)
-f1 = f1_score(y_test, y_pred, average="macro")
+    model.fit(X_train, y_train)
 
-print("Accuracy :", acc)
-print("Macro F1 :", f1)
+    y_pred = model.predict(X_test)
 
+    acc = accuracy_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred, average="macro")
 
-# ======================================================
-# SAVE MODEL (UNTUK DOCKER)
-# ======================================================
-joblib.dump(model, "model.pkl")
-print("Model saved as model.pkl")
+    mlflow.log_metric("accuracy", acc)
+    mlflow.log_metric("macro_f1", f1)
+
+    # 🔴 INI WAJIB ADA UNTUK BUILD DOCKER
+    mlflow.xgboost.log_model(model, artifact_path="model")
+
+    print("=== TRAINING SELESAI ===")
+    print("Experiment :", EXPERIMENT_NAME)
+    print(f"Accuracy   : {acc:.4f}")
+    print(f"Macro F1   : {f1:.4f}")
